91b688935410beab58d005eb56eb4832647d03e1
Christian DeWolf <christian@mirthturtle.com>
2024-11-26 18:37:19 -0400

    Add closing time

--- gym.rb
+++ gym.rb
@@ -1,7 +1,18 @@
 require 'net/http'
 require 'json'
+require 'date'
 
 scrape_success = false
+closing_times = {
+  0 => "9pm",
+  1 => "11pm",
+  2 => "11pm",
+  3 => "11pm",
+  4 => "11pm",
+  5 => "11pm",
+  6 => "9pm"
+}
+closes_at = closing_times[Date.today.wday]
 
 begin
   uri = URI("https://portal.rockgympro.com/portal/public/da1bdc14b9e219e81de152ec2576d858/occupancy?&iframeid=occupancyCounter&fId=1347")
@@ -25,7 +36,7 @@ if scrape_success
 
   puts "Gottingen:   #{json['SBG']['count']} / #{json['SBG']['capacity']}"
   puts "Bayers Lake: #{json['SBL']['count']} / #{json['SBL']['capacity']}"
-
+  puts "Closes at: #{closes_at}"
 else
   puts "Error getting gym numbers!"
 end
